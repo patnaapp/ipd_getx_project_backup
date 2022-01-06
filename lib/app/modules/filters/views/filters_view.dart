5a87489edce39bd75261a7bb076365de471de88a
Nagma <nagmakhan056@gmail.com>
2022-01-06 16:21:11 +0530

    filter added

--- lib/app/modules/filters/views/filters_view.dart
+++ lib/app/modules/filters/views/filters_view.dart
@@ -6,59 +6,126 @@ import '../controllers/filters_controller.dart';
 
 class FiltersView extends GetView<FiltersController> {
   @override
-  bool visibilityTag = false;
-  bool visibilityObs = false;
-
   Widget build(BuildContext context) {
+    Get.put(FiltersController());
     return Scaffold(
       backgroundColor: Color(0xffeaf1f9),
       body: SafeArea(
-        child: SingleChildScrollView(
-          //physics: NeverScrollableScrollPhysics(),
-          physics: ScrollPhysics(),
-          child: Container(
-            padding: const EdgeInsets.fromLTRB(10, 15, 10, 10),
-            margin: const EdgeInsets.only(left: 20.0, right: 20.0),
-            decoration: BoxDecoration(
+        child: Container(
+          margin: const EdgeInsets.only(top: 20),
+          decoration: BoxDecoration(
+              color: Colors.white,
+              border: Border.all(
                 color: Colors.white,
-                border: Border.all(
-                  color: Colors.white,
-                ),
-                borderRadius: BorderRadius.all(Radius.circular(20))),
-            child: Row(children: <Widget>[
-              Container(
-                child: GestureDetector(
-                  onTap: () {
-                    print(visibilityObs);
+              ),
+              borderRadius: BorderRadius.all(Radius.circular(20))),
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Padding(
+                padding: EdgeInsets.all(12),
+                child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Text(
+                        "Filters",
+                        style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold
+                        ),
+                      ),
 
-                      visibilityObs ? false : );
-
-                    print(visibilityObs);
-                    },
-                  child: Text(
-                    " Medicine ",
-                    style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
-
-                  ),
-                ),
+                      TextButton(
+                        onPressed: (){
+                          Get.back();
+                          //Handle Close Functionality
+                        },
+                        child: Text(
+                          "Close",
+                          style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.blue[500]
+                          ),
+                        ),
+                      )
+                    ]),
               ),
 
-              visibilityObs
-                  ? Container(
-                      child: Text(
-                        " Nagma ",
-                        style: TextStyle(
-                            fontSize: 28, fontWeight: FontWeight.bold),
+              SizedBox(height: 2,
+                  child: Container(decoration: BoxDecoration(color: Color(0xffeaf1f9)),)
+              ),
+
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  SizedBox(
+                    width: 150,
+                    child: GetBuilder<FiltersController>(
+                      builder: (_) =>
+                          ListView.builder(
+                            physics: ScrollPhysics(),
+                            scrollDirection: Axis.vertical,
+                            shrinkWrap: true,
+                            itemBuilder: (BuildContext context, int index) {
+                              return controller.buildListItem(
+                                  controller.filterList[index], context, index);
+                            },
+                            itemCount: controller.filterList.length,
+                          ),
+                    ),
+                  ),
+
+                  Expanded(
+                      child:
+                      GetBuilder<FiltersController>(
+                          builder: (_)=>
+                              ListView.builder(
+                                physics: ScrollPhysics(),
+                                scrollDirection: Axis.vertical,
+                                shrinkWrap: true,
+                                itemBuilder: (BuildContext context, int index) {
+                                  return controller.buildSubListItem(
+                                      controller.subCatList[index], context, index);
+                                },
+                                itemCount: controller.subCatList.length,
+                              )
+                      )
+                  ),
+
+                ],
+              ),
+              Expanded(
+                    child: Container(
+                      margin: EdgeInsets.all(8),
+                      width: double.infinity,
+                      child: Align(
+                          alignment: Alignment.bottomCenter,
+                        child: OutlinedButton(
+                          child: Text(
+                            " APPLY ",
+                            style: TextStyle(fontSize: 20.0),
+                          ),
+                          style: OutlinedButton.styleFrom(
+                              backgroundColor: Colors.white,
+                              primary: Colors.blue,
+                              minimumSize: Size(330,50),
+                              elevation: 2.0,
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(0))),
+
+                          onPressed: () async{
+                            //Get.to(
+                                  //() => ScanQRCodeView(),
+                           // );
+                          },
+                        ),
                       ),
                     )
-                  : Container(
-                child: Text(
-                  " parween",
-                  style: TextStyle(
-                      fontSize: 28, fontWeight: FontWeight.bold),
                 ),
-              )
-            ]),
+            ],
           ),
         ),
       ),
@@ -66,12 +133,3 @@ class FiltersView extends GetView<FiltersController> {
   }
 }
 
-void _changed(bool visibility, String field) {
-
-    if (field == "tag"){
-      visibilityTag = visibility;
-    }
-    if (field == "obs") {
-      visibilityObs = visibility;
-    }
-}
